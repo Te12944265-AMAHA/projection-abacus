eee73f91e27b7e52702019bc6d4356fac2e97883
Te12944265-AMAHA <1603699457@qq.com>
2020-06-18 13:05:27 -0400

    .

---
+++ SfM/features.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import scipy.optimize
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+from scipy.spatial import distance
+from scipy.ndimage import gaussian_filter
+from scipy.linalg import rq
+
+import skimage.color
+from helper import briefMatch
+from helper import computeBrief
+from helper import corner_detection
+
+def matchPics(I1, I2):
+	#I1, I2 : Images to match
+	#Convert Images to GrayScale
+	I1 = cv2.cvtColor(I1, cv2.COLOR_RGB2GRAY)
+	I2 = cv2.cvtColor(I2, cv2.COLOR_RGB2GRAY)
+	#Detect Features in Both Images
+	locs1 = corner_detection(I1)
+	locs2 = corner_detection(I2)
+	#Obtain descriptors for the computed feature locations
+	desc1, locs1 = computeBrief(I1, locs1)
+	desc2, locs2 = computeBrief(I2, locs2)
+	#Match features using the descriptors
+	
+	locs1 = np.flip(locs1, axis=1)
+	locs2 = np.flip(locs2, axis=1)
+	
+	matches = briefMatch(desc1, desc2, 0.8)
+	print(matches.shape)
+	return matches, locs1, locs2
--- SfM/pipeline.py
+++ SfM/pipeline.py
@@ -1,14 +1,24 @@
 import cv2
 import numpy as np 
+from features import *
+import util
 
-# Extract features from images
+## Extract features from images and match
+I1 = cv2.imread()
+I2 = cv2.imread()
+matches, loc1, locs2 = matchPics(I1, I2)
 
+# process them
+pts1 = None
+pts2 = None
 
-# Match features
-
-
-# Calculate F using 8-point alg
-
+## Calculate F using 8-point alg
+pts1 = np.int32(pts1)
+pts2 = np.int32(pts2)
+F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
+# We select only inlier points
+pts1 = pts1[mask.ravel()==1]
+pts2 = pts2[mask.ravel()==1]
 
 # Calculate P and P' from F
 
